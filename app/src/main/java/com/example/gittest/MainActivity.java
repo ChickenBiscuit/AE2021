f89e20683ff8145f855d9101f6f87af3abf204e4
ChickenBiscuit <flynn11224@gmail.com>
2021-05-03 10:20:55 +0200

    yeet

--- app/src/main/java/com/example/gittest/MainActivity.java
+++ app/src/main/java/com/example/gittest/MainActivity.java
@@ -28,6 +28,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+    //BAJFBAJOFWBFOJUA
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
